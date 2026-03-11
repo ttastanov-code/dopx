96c66110676ad70dd82b596ad25a4bb1f06e6e0b
tmtimur <t.tastanov@gmail.com>
2026-03-11 16:41:03 +0500

    stable state

--- seasons/models.py
+++ seasons/models.py
@@ -14,6 +14,12 @@ class Season(BaseModel):
     year = models.CharField(max_length=20)
 
     is_active = models.BooleanField(default=False)
+    external_id = models.CharField(
+        max_length=100,
+        unique=True,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return f"{self.league.name} {self.year}"
